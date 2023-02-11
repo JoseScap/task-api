2d8f0296e77f0a0938983f0ceecf61efda7bc79e
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2023-02-11 20:14:39 -0300

    ip serving

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  app.enableCors({ origin: '*' });
+  const port = process.env.PORT || 3000;
+  if (!process.env.IP) await app.listen(port, process.env.IP);
+  else await app.listen(port);
 }
 bootstrap();
